90d70122e68236529aa9aad8bf205df326ae601c
Ajay Hrishi V <ajayhrishi@gmail.com>
2024-03-28 23:39:03 +0530

    working on the backend Controller functions

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("intiated the work on backend Movie booking app");
+import express from 'express' // will try using require() later in the code to see the difference. 
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors'
+dotenv.config();
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());   
+mongoose.connect(`mongodb+srv://ajayhrishi:${process.env.db}@moviebooking.kjzzkly.mongodb.net/`).then(()=>console.log('DataBaseConnected'))
+.then(()=>{app.listen(5000,()=>{console.log("intiated the work on backend Movie booking app");})}).catch((err)=>{console.log(err)});
+
+
+
+
+
+
+// in package.json file the line //"start": "nodemon --experimental-modules --es-module-specifier-resolution=node app.js",  is only enabling the es6 to the project  
+// project passowrd uoKyNhj0u5MRWsYp

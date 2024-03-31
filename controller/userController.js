963674eebee1bcd2286db39cffa1eefee4ef1633
Ajay Hrishi V <ajayhrishi@gmail.com>
2024-03-31 06:40:08 +0530

    all the userController functions are finished

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,8 @@
-
+ 
 import User from "../model/User.js";
-
+import  bcrypt  from "bcryptjs";
 export const getAllUsers = async(req,res,next)=>{
+    
     let users;
     try{
         users = await User.find();
@@ -20,7 +21,7 @@ export const getAllUsers = async(req,res,next)=>{
 
 
 export const addUser = async(req,res,next)=>{
-    console.log('add user Function triggered');
+   
     const {name, email, password } = req.body;
     if(
         !name && name ==="" && !email && email==="" && !password && password===""
@@ -29,9 +30,9 @@ export const addUser = async(req,res,next)=>{
     }
 
     let user;
-
+    const cryptedPassword = bcrypt.hashSync(password); 
     try{
-        user = new User({name,email, password});
+        user = new User({name,email, password:cryptedPassword});
         user = await user.save();
     }catch(err){
         return next(err);
@@ -42,4 +43,74 @@ export const addUser = async(req,res,next)=>{
     }
 
     return res.status(201).json({user});
+}
+
+export const updateUser = async(req,res,next)=>{
+    const id = req.params.id;
+    const {name, email, password } = req.body;
+    if(
+        !name && name ==="" && !email && email==="" && !password && password===""
+    ){
+        return res.status(422).json({message:"Invalid data"});
+    }
+
+    let user;
+    const cryptedPassword = bcrypt.hashSync(password); 
+    try{
+        user = await User.findByIdAndUpdate(id,{
+            name, email, password: cryptedPassword
+        });
+    }catch(err){
+        return next(err); 
+    }
+    if(!user){
+        return res.status(500).json({message:"Internal Error"});
+    }
+
+    return res.status(201).json({user});
+}
+
+export const deleteUser = async(req,res,next) =>{
+    let user;
+    try{
+    user = await User.findByIdAndDelete(req.params.id)
+}catch(err){
+    console.log(err);
+    return res.status(404).json({message:'unable to delete'});
+}
+if(!user){
+    return res.status(500).json({message:"Internal Server Error"});
+}
+
+return res.status(201).json({message:'deleted'})
+}
+
+
+export const loginUser = async(req,res,next) =>{
+    
+    let {email, password } = req.params; 
+    
+    if(
+       !email && email==="" && !password && password===""
+    ){
+        return res.status(422).json({message:"Invalid data"});
+    }
+    let existingUser;
+    try{
+        existingUser = await User.findOne(email);
+    }catch(err){
+        return console.log(err);
+    }
+
+    if(!existingUser){
+        return res.status(404).json({message:'unable to find user with this email'});
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password,existingUser.password);
+
+    if(!isPasswordCorrect){
+        return res.status(400).json({message:'Incorrect Password'}); 
+    }
+
+    return res.status(201).json({message:'loggedIn', id});
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import {addUser, getAllUsers} from '../controller/userController.js'
+import {addUser, deleteUser, getAllUsers, loginUser, updateUser} from '../controller/userController.js'
 
 const userRouter = express.Router();
 
-userRouter.get('/allUsers', getAllUsers);
+userRouter.get('/', getAllUsers);
 userRouter.post('/signUp', addUser);
-
-
-
+userRouter.put('/:id', updateUser);
+userRouter.delete('/:id', deleteUser);
+userRouter.post('/login', loginUser)
 export default userRouter;

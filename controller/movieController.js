5dd0c46ae021f7bb2ae566b38ceea6cc7233a5f7
Ajay Hrishi V <ajayhrishi@gmail.com>
2024-04-07 18:54:11 +0530

    movies testing

--- controller/movieController.js
+++ controller/movieController.js
@@ -1,21 +1,34 @@
 import Movie from "../model/Movie.js";
+import mongoose from "mongoose";
+
 import jwt from "jsonwebtoken"; 
 
 
 
 
 export const getAllMovies = async(req,res,next)=>{
-    console.log('testing');
-
-
+    let movies;
+    try{
+        movies = await Movie.find();
+    }catch(err){
+        console.log(err);
+       return res.status(404).jason({message:"No movies found"}); 
+    }
+    if(!movies){
+        console.log('there is no movies inside');
+    }
+    return res.status(200).json({movies});
 
 }
 
 
+
 export const addMovie = async(req,res,next)=>{
-
-
-
+    const extractedToken = req.headers.autharization;
+    if(!extractedToken&&extractedToken===""){
+        return res.status(404).jason({message: "Token not found"})
+    }
+    console.log(extractedToken);
     
 }
 
